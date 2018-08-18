92e00daccaafa2d6b8925e3e4a95b7f78ba9d165
Amaan Khan <amaancan@gmail.com>
2018-08-18 19:53:03 -0400

    Initial commit: iPad set-up

---
+++ Views/CanvasView.swift
@@ -0,0 +1,20 @@
+//
+//  CanvasView.swift
+//  DragNDropArtist
+//
+//  Created by Amaan on 2018-08-18.
+//  Copyright © 2018 amaancan. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+
+}

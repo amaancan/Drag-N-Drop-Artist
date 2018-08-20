aa6559281ac0f23aa07b6c38589d322938d060c5
Amaan Khan <amaancan@gmail.com>
2018-08-20 00:46:26 -0400

    Add drop interaction to canvasView

--- Views/CanvasView.swift
+++ Views/CanvasView.swift
@@ -15,10 +15,52 @@ class CanvasView: UIView { // Question: Should be an ImageView?
             setNeedsDisplay() // calls draw(_ rect)
         }
     }
-
+    
     override func draw(_ rect: CGRect) {
         backgroundImage?.draw(in: bounds)
     }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+}
 
-
+extension CanvasView: UIDropInteractionDelegate {
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+        // TODO: abliity to move previously dropped emojis around w/in the image
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSAttributedString.self) { (providers) in
+            let dropPoint = session.location(in: self)
+            
+            for draggedEmoji in providers as? [NSAttributedString] ?? [] {
+                self.addLabel(with: draggedEmoji, centeredAt: dropPoint)
+            }
+        }
+    }
+    
+    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = point
+        
+        addSubview(label)
+    }
 }
